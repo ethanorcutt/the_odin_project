2690e0a23ba27b8ee9927228784855e7c2689ea9
Ethan Orcutt <izzaboy306@gmail.com>
2018-10-15 22:00:05 -0500

    Completed the calculator and palindromes.. Caesar is hard lol.

--- javascript-exercises/caesar/caesar.js
+++ javascript-exercises/caesar/caesar.js
@@ -1,5 +1,18 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+  let charArray = string.split('');
+  let shiftedArray = [];
 
+  console.log(charArray);
+
+  for (var i = 0; i < charArray.length; i++) {
+    if(isLetter(charArray[i])) shiftedArray.push(String.fromCharCode((charArray[i].charCodeAt() + shift)));
+  }
+  
+  return shiftedArray.join('');
+}
+
+function isLetter(c) {
+  return c.toLowerCase() != c.toUpperCase();
 }
 
 module.exports = caesar
