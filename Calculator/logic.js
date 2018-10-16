96b8f3d984acd30220007b8d2308de1d5657878e
Ethan Orcutt <izzaboy306@gmail.com>
2018-10-16 14:24:39 -0500

    Started on the calculator. Have about 80% funtionality now.

---
+++ Calculator/logic.js
@@ -0,0 +1,103 @@
+const container = document.querySelector('.container');
+const calculatorHead = document.querySelector('#calculator-head');
+const calculatorBody = document.querySelector('#calculator-body');
+const ignoreableIDs = ['clear-all', 'clear-current', 'backspace', 'pos-neg-toggle', '='];
+const gridWidth = 450;
+const gridSize = 4;
+
+let calculatorOutput = 0;
+let gridBlocks;
+let inputtedValues = [];
+
+function add (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function operate () {
+  const operators = ['+', '-', '/', '*'];
+  let operatorIndices = [];
+  let firstValue = 0;
+  let secondValue = 0;
+  let operator = '';
+  
+  for (let x = 0; x <= inputtedValues.length; x++) {
+    if (operators.includes(inputtedValues[x])) operatorIndices.push(x);
+  }
+
+  console.log(inputtedValues);
+  console.log(operatorIndices);
+
+  firstValue = Number(inputtedValues.slice(0, operatorIndices[0]).join(''));
+  secondValue = Number(inputtedValues.slice(operatorIndices[0]
+              + 1, inputtedValues.length).join(''));
+  operator = inputtedValues[operatorIndices[0]];
+
+  switch (operator) {
+    case '+': calculatorOutput = add (firstValue, secondValue); break;
+    case '-': calculatorOutput = subtract (firstValue, secondValue); break;
+    case '*': calculatorOutput = multiply (firstValue, secondValue); break;
+    case '/': calculatorOutput = divide (firstValue, secondValue); break;
+    default: alert('Invalid Input');
+  }
+
+  checkAndUpdateScores ();
+}
+
+function createGrid (maxHeightWidth) {
+  calculatorBody.style.gridTemplateColumns = `repeat(${gridSize}, ${maxHeightWidth})`;
+  calculatorBody.style.gridTemplateRows = `repeat(${gridSize}, ${maxHeightWidth})`;
+}
+
+function createBoard () {
+  let maxHeightWidth = (gridWidth / gridSize);
+  let divCount = Math.pow(gridSize, 2);
+
+  createGrid(maxHeightWidth);
+  checkAndUpdateScores();
+
+  gridBlocks = document.querySelectorAll('#calculator-body .grid-block-divs');
+  addEventListenersToGridBlocks();
+}
+
+function updateTextDisplays (target, string) {
+    const calculatorOutput = document.querySelector('#calculator-output');
+    
+    if(target === 'calculatorOutput') calculatorOutput.textContent = string;
+}
+
+function checkAndUpdateScores () {
+    updateTextDisplays('calculatorOutput', calculatorOutput);
+}
+
+function addEventListenersToGridBlocks () {
+  gridBlocks.forEach((gridBlock) => {
+    gridBlock.addEventListener('click', (e) => {
+      if (!ignoreableIDs.includes(gridBlock.id)) {
+        inputtedValues.push(gridBlock.id);
+      }
+
+      if (gridBlock.id == 'clear-all') {
+        calculatorOutput = 0;
+        inputtedValues = [];
+        checkAndUpdateScores();
+      }
+      else if (gridBlock.id == '='){
+        operate ();
+      }
+    });
+  });
+}
+
+createBoard ();
