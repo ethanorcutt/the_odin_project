41e0e4541e04057dbcf61ced1a66eb5f298037e4
Ethan Orcutt <izzaboy306@gmail.com>
2018-10-15 22:00:34 -0500

    Completed the calculator and palindromes.. Caesar is hard lol.

--- javascript-exercises/calculator/calculator.js
+++ javascript-exercises/calculator/calculator.js
@@ -1,25 +1,34 @@
-function add () {
-	
+function add (x1, x2) {
+	return x1 + x2;
 }
 
-function subtract () {
-	
+function subtract (x1, x2) {
+	return x1 - x2;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((a, b) => a + b, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	// Has to be 1 as the default b/c the first run it 0 * 2 => 0.
+	// By setting default to 1, we let the problem continue. 
+	return array.reduce((a, b) => a * b, 1);
 }
 
-function power() {
-	
+function power(x1, x2) {
+	return Math.pow(x1, x2);
 }
 
-function factorial() {
-	
+function factorial(x1) {
+	if (x1 == 0 || x1 == 1) return 1;
+	let product = 1;
+
+	for (var i = x1; i > 0; i--) {
+		product *= i;
+	}
+
+	return product;
 }
 
 module.exports = {
--- javascript-exercises/palindromes/palindromes.js
+++ javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
-
+const palindromes = function(input) {
+  processedInput = input.toLowerCase().replace(/[^A-Za-z]/g, "");
+  return processedInput === processedInput.split('')
+                                  .reverse()
+                                  .join('') ? true : false;
 }
 
 module.exports = palindromes
